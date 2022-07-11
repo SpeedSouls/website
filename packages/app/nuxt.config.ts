7623282ba171b9b27ee4011304b762133279caa9
CapitaineToinon <antoinesutter@gmail.com>
2022-07-11 16:45:37 +0200

    Switched to pnpm monorepo

--- packages/app/nuxt.config.ts
+++ packages/app/nuxt.config.ts
@@ -3,7 +3,7 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
 	css: ['@/assets/tailwind.css'],
-	// ssr: false,
+	ssr: false,
 	postcss: {
 		plugins: {
 			tailwindcss: {},
